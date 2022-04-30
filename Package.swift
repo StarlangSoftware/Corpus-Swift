a48a44e666068a0e46339c81ccdb317911fbed40
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-04-30 11:44:57 +0300

    Updated Dictionary dependency.

--- Package.swift
+++ Package.swift
@@ -12,16 +12,15 @@ let package = Package(
             targets: ["Corpus"]),
     ],
     dependencies: [
-        .package(name: "Dictionary", url: "https://github.com/StarlangSoftware/Dictionary-Swift.git", .exact("1.0.10")),
+        .package(name: "Dictionary", url: "https://github.com/StarlangSoftware/Dictionary-Swift.git", .exact("1.0.11")),
         .package(name: "DataStructure", url: "https://github.com/StarlangSoftware/DataStructure-Swift.git", .exact("1.0.4")),
-        .package(name: "Util", url: "https://github.com/StarlangSoftware/Util-Swift.git", .exact("1.0.8")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Corpus",
-            dependencies: ["Dictionary", "DataStructure", "Util"],
+            dependencies: ["Dictionary", "DataStructure"],
             resources: [.process("corpus.txt"),.process("simplecorpus.txt")]),
         .testTarget(
             name: "CorpusTests",
